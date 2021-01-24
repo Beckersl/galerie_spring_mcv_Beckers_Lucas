279c166678fd76772144080dbcf91265fb5de7e8
Lucas Beckers <>
2021-01-24 20:42:07 +0100

    Commit des questions

--- src/main/java/galerie/controller/TableauController.java
+++ src/main/java/galerie/controller/TableauController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import galerie.dao.GalerieRepository;
+
 import galerie.dao.TableauRepository;
-import galerie.entity.Galerie;
+import galerie.dao.ArtisteRepository;
+import galerie.entity.Tableau;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,12 +20,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  * Edition des catégories, sans gestion des erreurs
  */
 @Controller
-@RequestMapping(path = "/galerie")
-public class GalerieController {
+@RequestMapping(path = "/tableau")
+public class TableauController {
 
     @Autowired
-    private GalerieRepository dao;
-       
+    private TableauRepository dao;
+
 
     /**
      * Affiche toutes les catégories dans la base
@@ -33,41 +34,42 @@ public class GalerieController {
      * @return le nom de la vue à afficher ('afficheGaleries.html')
      */
     @GetMapping(path = "show")
-    public String afficheToutesLesGaleries(Model model) {
-        model.addAttribute("galeries", dao.findAll());
-        return "afficheGaleries";
+    public String afficheTousLesTableaux(Model model) {
+        model.addAttribute("tableaux", dao.findAll());
+        return "afficheTableaux";
     }
 
     /**
      * Montre le formulaire permettant d'ajouter une galerie
      *
-     * @param galerie initialisé par Spring, valeurs par défaut à afficher dans le formulaire
+     * @param tableau initialisé par Spring, valeurs par défaut à afficher dans le formulaire
      * @return le nom de la vue à afficher ('formulaireGalerie.html')
      */
     @GetMapping(path = "add")
-    public String montreLeFormulairePourAjout(@ModelAttribute("galerie") Galerie galerie) {
-        return "formulaireGalerie";
+    public String montreLeFormulairePourAjout(@ModelAttribute("tableau") Tableau tableau, Model model) {
+        model.addAttribute("artistes", dao.findAll());
+        return "formulaireTableau";
     }
 
     /**
      * Appelé par 'formulaireGalerie.html', méthode POST
      *
-     * @param galerie Une galerie initialisée avec les valeurs saisies dans le formulaire
+     * @param tableau Une galerie initialisée avec les valeurs saisies dans le formulaire
      * @param redirectInfo pour transmettre des paramètres lors de la redirection
      * @return une redirection vers l'affichage de la liste des galeries
      */
     @PostMapping(path = "save")
-    public String ajouteLaGaleriePuisMontreLaListe(Galerie galerie, RedirectAttributes redirectInfo) {
+    public String ajouteLeTableauPuisMontreLaListe(Tableau tableau, RedirectAttributes redirectInfo) {
         String message;
         try {
             // cf. https://www.baeldung.com/spring-data-crud-repository-save
-            dao.save(galerie);
+            dao.save(tableau);
             // Le code de la catégorie a été initialisé par la BD au moment de l'insertion
-            message = "La galerie '" + galerie.getNom() + "' a été correctement enregistrée";
+            message = "Le tableau'" + tableau.getTitre() + "' a été correctement enregistrée";
         } catch (DataIntegrityViolationException e) {
             // Les noms sont définis comme 'UNIQUE' 
             // En cas de doublon, JPA lève une exception de violation de contrainte d'intégrité
-            message = "Erreur : La galerie '" + galerie.getNom() + "' existe déjà";
+            message = "Erreur : Le tableau '" + tableau.getTitre() + "' existe déjà";
         }
         // RedirectAttributes permet de transmettre des informations lors d'une redirection,
         // Ici on transmet un message de succès ou d'erreur
@@ -79,19 +81,19 @@ public class GalerieController {
     /**
      * Appelé par le lien 'Supprimer' dans 'afficheGaleries.html'
      *
-     * @param galerie à partir de l'id de la galerie transmis en paramètre, Spring fera une requête SQL SELECT pour
+     * @param tableau à partir de l'id de la galerie transmis en paramètre, Spring fera une requête SQL SELECT pour
      * chercher la galerie dans la base
      * @param redirectInfo pour transmettre des paramètres lors de la redirection
      * @return une redirection vers l'affichage de la liste des galeries
      */
     @GetMapping(path = "delete")
-    public String supprimeUneCategoriePuisMontreLaListe(@RequestParam("id") Galerie galerie, RedirectAttributes redirectInfo) {
-        String message = "La galerie '" + galerie.getNom() + "' a bien été supprimée";
+    public String supprimeUneCategoriePuisMontreLaListe(@RequestParam("id") Tableau tableau, RedirectAttributes redirectInfo) {
+        String message = "Le Tableau '" + tableau.getTitre() + "' a bien été supprimée";
         try {
-            dao.delete(galerie); // Ici on peut avoir une erreur (Si il y a des expositions pour cette galerie par exemple)
+            dao.delete(tableau); // Ici on peut avoir une erreur (Si il y a des expositions pour cette galerie par exemple)
         } catch (DataIntegrityViolationException e) {
             // violation de contrainte d'intégrité si on essaie de supprimer une galerie qui a des expositions
-            message = "Erreur : Impossible de supprimer la galerie '" + galerie.getNom() + "', il faut d'abord supprimer ses expositions";
+            message = "Erreur : Impossible de supprimer le tableau '" + tableau.getTitre() + "', il faut d'abord supprimer son auteur";
         }
         // RedirectAttributes permet de transmettre des informations lors d'une redirection,
         // Ici on transmet un message de succès ou d'erreur
